7c034695f874a8933ef924cd753b1c21b82c42af
Kyle <kaisonpham@gmail.com>
2025-09-18 01:01:08 -0700

    Updated example config file

--- example_config.py
+++ example_config.py
@@ -3,4 +3,4 @@ CHECKPOINT_DIR = "checkpoints/openaudio-s1-mini"
 CHECKPOINT_PATH = "checkpoints/openaudio-s1-mini/codec.pth"
 MODEL_CONFIG = "modded_dac_vq"
 AUDIO_DEVICES = [0] # deviceid (int) or device name (str)
-MODEL = "example.npy"
+MODEL = "example" # no file extension!
